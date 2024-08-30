991a8cf45e845c83751e067da49020f7d9750a96
Christopher Soo <chitosux@gmail.com>
2024-08-30 16:59:05 +0100

    changed pipeline codes

--- pipeline/extractdata.py
+++ pipeline/extractdata.py
@@ -1,11 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = r'https://www.ebay.co.uk/sch/i.html?_from=R40&_trksid=p3711649.m570.l1313&_nkw=laptop&_sacat=0'
+init_page = 1
+max_page = 9
 
-response = requests.get(url)
+def extract_data(page):
+    url = f'https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=laptop&_sacat=0&_ipg=240&_pgn={page}'
 
-if response.status_code != 200:
-    raise ValueError("Failed to reach webpage")
+    response = requests.get(url)
 
-soup = BeautifulSoup()
+    if response.status_code != 200:
+        raise ValueError("Failed to reach webpage")
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    listings = soup.find_all('li', class_='s-item')
+
+    for listing in listings:
+        name = listing.find('div', class_='s-item__title')
+        price = listing.find('span', class_='s-item__price')
+        if name and price:
+            print(f"Laptop: {name.text}, Price: {price.text}")
+
+extract_data(1)
